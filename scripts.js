9f75cc9378abca8957948ae56a51e44f8f9df7a7
jugcf <juliagalindo030604@gmail.com>
2026-01-29 14:01:23 -0300

    Correção de erros no JavaScript

--- scripts.js
+++ scripts.js
@@ -2,47 +2,50 @@ const prevButton = document.getElementById('prev')
 const nextButton = document.getElementById('next')
 const items = document.querySelectorAll('.item')
 const dots = document.querySelectorAll('.dot')
-const numberIndicator = document.querySelector('numbers')
-const list = document.querySelector('.list')
+const numberIndicator = document.querySelector('.numbers')
 
-let active = 0;
-const total = items.lenght
-let timer;
+let active = 0
+const total = items.length
+let timer
 
 function update(direction) {
+    // remove ativo atual
     document.querySelector('.item.active').classList.remove('active')
-    document.querySelector('.dot.active').classList.remove('.active')
+    document.querySelector('.dot.active').classList.remove('active')
 
-    if(direction > 0){
-        active = active +1
-
-        if(active === total){
-            active = 0
-        }
-
-    } else if(direction < 0){
-        active = active - 1
-
-        if(active < 0){
-            active = total - 1
-        }
+    // controla índice
+    if (direction > 0) {
+        active++
+        if (active === total) active = 0
+    } else if (direction < 0) {
+        active--
+        if (active < 0) active = total - 1
     }
 
+    // adiciona novo ativo
     items[active].classList.add('active')
     dots[active].classList.add('active')
 
-    numberIndicator.textContent = String(active +1).padStart(2,'0')
+    // atualiza número
+    numberIndicator.textContent = String(active + 1).padStart(2, '0')
+
+    resetTimer()
 }
 
+function resetTimer() {
     clearInterval(timer)
-    timer = setInterval (() => {
-        update (1)
-    },5000);
+    timer = setInterval(() => {
+        update(1)
+    }, 5000)
+}
 
-prevButton.addEventListener('click', function(){
+// autoplay inicial
+resetTimer()
+
+prevButton.addEventListener('click', () => {
     update(-1)
 })
 
-nextButton.addEventListener('click', function(){
+nextButton.addEventListener('click', () => {
     update(1)
 })
